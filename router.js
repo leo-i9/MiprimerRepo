f56fee94bc00a94d13a6ba1f9715fb5688cf0b57
Nemesis <sauulventura690@gmail.com>
2024-05-10 21:13:34 -0600

    5

--- router.js
+++ router.js
@@ -58,7 +58,7 @@ router.get("/buscar",async(req,res)=>{
 
 router.get("/",(req,res)=>{
   res.header('Access-Control-Allow-Origin', `*`); // Permite a todos los dominios
-  res.header('Access-Control-Allow-Methods', 'PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.send({todo:"bien"})
+    res.send({todo:"bien :)"})
 })

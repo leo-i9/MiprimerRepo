2a51b0fdccebbd365e771d947dced8853e0f3ef8
Nemesis <sauulventura690@gmail.com>
2024-05-11 02:29:08 -0600

    e--

--- router.js
+++ router.js
@@ -15,9 +15,9 @@ async function pp(n1="",n2=""){
         "--disable-features=BlockInsecurePrivateNetworkRequests"
     ]
   });
-
+   
   const page = await browser.newPage();
-  
+  await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36");
   // Habilitar la interceptación de solicitudes
   await page.setRequestInterception(true);
   
